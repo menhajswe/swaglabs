2fddf5b0c90481f57a38e50c3eccad9fa5718ffb
menhajswe <menhajswe@gmail.com>
2024-12-05 23:42:47 -0500

    added logout test

--- src/test/java/loginpage/TestLogin.java
+++ src/test/java/loginpage/TestLogin.java
@@ -7,10 +7,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 
 public class TestLogin {
+    private static final Logger log = LoggerFactory.getLogger(TestLogin.class);
     WebDriver driver;
 
     @BeforeEach
@@ -26,6 +29,20 @@ public class TestLogin {
 
     @Test
     public void testLogin() {
+        // confirm url is inventory
+        login();
+        Assertions.assertEquals("https://www.saucedemo.com/inventory.html", driver.getCurrentUrl());
+    }
+
+    @Test
+    public void testLogout() {
+        login();
+        logout();
+        Assertions.assertEquals("https://www.saucedemo.com/", driver.getCurrentUrl());
+
+    }
+
+    private void login() {
         By userIdPath = By.xpath("//*[@id=\"user-name\"]");
         By passwordPath = By.xpath("//input[@id='password']");
         By loginButton = By.xpath("//input[@id='login-button']");
@@ -46,8 +63,20 @@ public class TestLogin {
         } catch (InterruptedException ex) {
             System.out.println(ex.getMessage());
         }
+    }
 
-        // confirm url is inventory
-        Assertions.assertEquals("https://www.saucedemo.com/inventory.html", driver.getCurrentUrl());
+    private void logout() {
+        By menuPath = By.xpath("//button[@id='react-burger-menu-btn']");
+        WebElement menuElement = driver.findElement(menuPath);
+        menuElement.click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        By buttonPath = By.xpath("//a[@id='logout_sidebar_link']");
+        driver.findElement(buttonPath).click();
+
     }
 }
